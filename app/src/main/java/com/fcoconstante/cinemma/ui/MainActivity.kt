7e3d879e609c1e803f8cb8a6eebc6f07a98c60df
Javier Constante <javier@brounie.com>
2019-09-23 09:58:21 -0700

    First Commit - Project Setup and Libraries Added

---
+++ app/src/main/java/com/fcoconstante/cinemma/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fcoconstante.cinemma.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fcoconstante.cinemma.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
